2115aef29b6b307bde1deb4e275dd9fec36199d3
Brandon H Bodine <brandonhbodine@gmail.com>
2016-09-20 20:37:23 -0600

    Configuring Gulp, Browser Sync, and project structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,25 @@
 'use strict';
-
 var gulp = require('gulp');
-
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-	console.log('Default task running...');
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    server: "./app"
+  });
+
+  gulp.watch("app/scss/*.scss", ['sass']);
+  gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("app/scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.stream());
 });
+
+gulp.task('default', ['serve']);
