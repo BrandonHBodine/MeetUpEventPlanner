267d6298be8017a29dc4b9a42632b1cc4844c0d4
Brandon H Bodine <brandonhbodine@gmail.com>
2016-09-29 22:35:14 -0600

    More work on the build process. Moving on to HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 'use strict';
 var gulp = require('gulp');
+
+// Utilities
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
 var pump = require('pump');
+
+// CSS
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+// JS
+var uglify = require('gulp-uglify');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -21,16 +29,30 @@ gulp.task('sass', function() {
   return gulp.src("app/scss/*.scss")
     .pipe(sass())
     .pipe(gulp.dest("app/css"))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(browserSync.stream());
 });
 
 // Compress JS
-gulp.task('compress', function() {
+gulp.task('min-javascript', function() {
   return gulp.src('app/js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
 });
 
+// CSS
+gulp.task('min-css', function() {
+  return gulp.src("app/scss/*.scss")
+    .pipe(sass())
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest('dist/css'));
+});
+
 // Tasks to run
-gulp.task('build', ['compress']);
+gulp.task('build', ['min-javascript', 'min-css']);
 gulp.task('default', ['serve']);
